0a50e3c772659828b91e0b2f603ca00ad1d1bdcd
75Engel <106105557+75Engel@users.noreply.github.com>
2023-10-07 20:07:42 +0200

    update sql&engineer notebooks
    upload tags.csv

---
+++ src/Utils/functions.py
@@ -0,0 +1,15 @@
+def sql_query(query):
+    ''''
+    Generación de dataframe a partir de una base de datos
+    '''
+
+    # Ejecuta la query
+    cursor.execute(query)
+
+    # Almacena los datos de la query 
+    ans = cursor.fetchall()
+
+    # Obtenemos los nombres de las columnas de la tabla
+    names = [description[0] for description in cursor.description]
+
+    return pd.DataFrame(ans,columns=names)
